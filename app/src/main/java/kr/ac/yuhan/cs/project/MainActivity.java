3f0169de7607f484505d4ac8f1e7269a812151ce
Lee <ehdgus2580@naver.com>
2024-04-02 15:31:22 +0900

    git commit

--- app/src/main/java/kr/ac/yuhan/cs/project/MainActivity.java
+++ app/src/main/java/kr/ac/yuhan/cs/project/MainActivity.java
@@ -2,7 +2,10 @@ package kr.ac.yuhan.cs.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button developer_info_btn = (Button) findViewById(R.id.button1);
+        developer_info_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Btn1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button name_rule_btn = (Button) findViewById(R.id.button2);
+        name_rule_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Btn2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
